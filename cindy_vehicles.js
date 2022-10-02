739dc8ccc24f05a195d2a47caa28520b209ee390
cvu232 <kuroshiruchan@gmail.com>
2022-10-02 03:30:47 -0400

    Update cindy_vehicles.js

--- cindy_vehicles.js
+++ cindy_vehicles.js
@@ -1,6 +1,6 @@
 function createvehiclesCindy() {
     let vehicleCindy = {
-        position: [200, 200],
+        position: [Math.random() * 500, Math.random() * 200],
         velocity: [2, 2],
         size: 50,
         direction: 0,
@@ -17,7 +17,18 @@ function createvehiclesCindy() {
   
   function processCindysvehicles() {
     for (let vehicle of vehicles) {
-      if (vehicle.owner == "Cindy") {
+        if (vehicle.owner == "Cindy") {
+            let positions = vehicles.filter(function(not) {return not.owner !== "Cindy"})
+            let vcentTosensdist = Math.sqrt(1.5 * 1.5 + 1.5 * 1.5);
+            let vcentTosensangle = Math.PI / 4;
+            //borrowed & modified code from https://codepen.io/donwalsin/pen/rNvWqKR
+            for (i = 0; i < positions.length; i++) {
+                positions[i].distance = positions[i].position[1] - vehicle.position[1] * vcentTosensdist *
+                  Math.cos(vcentTosensangle),
+                  positions[i].position[0] - vehicle.position[0] + vcentTosensdist *
+                  Math.sin(vcentTosensangle)
+        }
+
         if (vehicle.position[1] <= 0) {
           vehicle.velocity[1] = vehicle.velocity[1] * -1;
           vehicle.direction = vehicle.direction * -1;
@@ -32,29 +43,11 @@ function createvehiclesCindy() {
           vehicle.direction = vehicle.direction * -1;
         }
   
-        /*for (let othervehicle of vehicles) {
-          if (vehicle.owner != "Cindy") {
-            if (
-              vehicle.position[0] >= othervehicle.position[0] - 10 &&
-              vehicle.position[0] <= othervehicle.position[0] + 10 &&
-              vehicle.position[1] >= othervehicle.position[1] - 10 &&
-              vehicle.position[1] <= othervehicle.position[1] + 10
-            ) {
-              vehicle.velocity[1] = vehicle.velocity[1] * -1;
-              vehicle.direction = vehicle.direction * -1;
-            }
-          }
-        }*/
-  
-        //if (vehicle.owner != "Cindy") {
-        //collision();
-        //}
-        //console.log("a");
+
         vehicle.position[0] += vehicle.velocity[0];
         vehicle.position[1] += vehicle.velocity[1];
   
-        ctx.save();
-        {
+        ctx.save(); {
           ctx.translate(vehicle.position[0], vehicle.position[1]);
           ctx.rotate(vehicle.direction);
           ctx.scale(vehicle.size, vehicle.size);
@@ -62,7 +55,12 @@ function createvehiclesCindy() {
           ctx.fillRect(-0.5, -0.5, 1, 1);
         }
         ctx.restore();
+        positions.forEach((not) => {
+            if(not.distance <= 50){
+                vehicle.position[0] = Math.random() * 500;
+                vehicle.position[1] = Math.random() * 200;
+            }
+        })
       }
     }
-  }
-  
+}
