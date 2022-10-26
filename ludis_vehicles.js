c2bb09cb3e543ad7532c3544213734419d2c92d8
viji5369 <ludwig.david.lorenz@uni-weimar.de>
2022-10-26 14:48:20 -0400

    adrenalin

--- ludis_vehicles.js
+++ ludis_vehicles.js
@@ -1,8 +1,18 @@
-function v_add(v1, v2) {
+  function v_add(v1, v2) {
+    v1 = [...v1]
+    v2 = [...v2]
     for (let i = 0; i < v1.length; i++) { 
       v1[i] = v1[i] + v2[i];
     }
     return v1;
+  }
+
+  function v_length(v) {
+    return Math.sqrt(v[0]**2 + v[1]**2)
+  }
+
+  function v_sub(v1, v2) {
+    return v_add(v1, vs_mult(v2, -1));
   }
 
   function v_to_degree(v) {
@@ -14,6 +24,7 @@ function v_add(v1, v2) {
   }
   
   function vs_mult(v1, s) {
+    v1 = [...v1]
     for (let i = 0; i < v1.length; i++) { 
       v1[i] = v1[i]*s;
     }
@@ -35,10 +46,33 @@ function v_add(v1, v2) {
     return x_collision && y_collision;
   }
 
+function ludi_scan_for_vehicles(my_vehicle) {
+  var adrenalin = false;
+  for (let vehicle of vehicles) {
+    if (vehicle.owner == "Alex") {
+      v_diff = v_sub(vehicle.position, my_vehicle.position)
+      if (v_length(v_diff) <= 70) {
+        my_vehicle.direction = v_to_degree(vs_mult(v_diff, -1));
+        my_vehicle.speed += 0.02;
+        adrenalin = true;
+        if (v_length(v_diff) <= 10) ludi_teleport(my_vehicle)
+      }
+    }
+  }
+  if (!adrenalin && my_vehicle.speed >= 1) {
+    my_vehicle.speed -= 0.01;
+  } 
+}
+
+function ludi_teleport(vehicle) {
+  vehicle.position[0] = Math.random() * window.innerWidth;
+  vehicle.position[1] = Math.random() * window.innerHeight;
+}
+
 function createvehiclesLudi() {
     for (let i = 0; i < 5; i++) {
       let vehicle = {
-        position: [20 + Math.random()*200, 300],
+        position: [20 + Math.random()*(canvas.width - 40), 20 + Math.random()*(canvas.height - 40)],
         velocity: [0, 0],
         size: 20,
         direction: Math.random(),
@@ -47,6 +81,7 @@ function createvehiclesLudi() {
         shape: 0,
         owner: "Ludi",
         colour: "green",
+        true_owner: "Ludi",
         id: String(i),
         history: []
       };
@@ -59,12 +94,15 @@ function processLudisVehicles() {
     for (let vehicle of vehicles) {
       if (vehicle.owner == "Ludi") {
         // Border detection
+        
         if ((vehicle.position[1] > (canvas.height - vehicle.size/2)) | (vehicle.position[1] < vehicle.size/2)) {
             vehicle.direction = -vehicle.direction;
         } else if ((vehicle.position[0] > (canvas.width- vehicle.size/2)) | (vehicle.position[0] < vehicle.size/2)) {
             vehicle.direction = (Math.PI -  vehicle.direction);
         }  
         
+        ludi_scan_for_vehicles(vehicle);
+
         var collission_detected = false;
         for (let other_vehicle of vehicles) {
             if (vehicle.id != other_vehicle.id) {
