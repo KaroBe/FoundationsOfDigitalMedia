d57168f52bf12b3dd6edcbcb6734304e50e91e7f
flymelon7 <114621219+flymelon7@users.noreply.github.com>
2022-11-23 15:23:44 -0500

    Update process.js

--- process.js
+++ process.js
@@ -30,7 +30,7 @@ function animate(){
     processJacobsvehicles();
     processCindysvehicles();
     //processDonsvehicles();
-	xiaosvehicle();
+	xiao_vehicle();
 
     // Initiate next frame
     window.requestAnimationFrame(animate);
