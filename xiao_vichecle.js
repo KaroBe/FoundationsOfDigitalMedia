5cbf0cdbc4cc3380750464dea5009418d0201c84
flymelon7 <114621219+flymelon7@users.noreply.github.com>
2022-11-23 15:10:37 -0500

    Create xiao_vichecle.js

---
+++ xiao_vichecle.js
@@ -0,0 +1,26 @@
+var angle = 0.0;
+var speed = 0.05;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	noFill( );
+}
+
+function draw() {
+
+  
+  
+  background(255);
+  
+  translate(width/2, height/2);
+  rotate(angle);
+  for (var i=0; i<5; i++) {
+	push();
+	rotate(i*TWO_PI/5);
+	translate(0, 10);
+	ellipse(0, 0, 20, 20);
+	pop();
+  }
+
+  angle += speed;
+}
